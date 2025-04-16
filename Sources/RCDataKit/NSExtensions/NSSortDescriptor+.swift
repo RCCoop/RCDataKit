5ac9b7b78b027b254b975d0cc8f8cd44b746284f
RCCoop <guthookhikes@gmail.com>
2025-04-16 17:11:31 -0400

    Update NSSortDescriptor+.swift

--- Sources/RCDataKit/NSExtensions/NSSortDescriptor+.swift
+++ Sources/RCDataKit/NSExtensions/NSSortDescriptor+.swift
@@ -15,8 +15,26 @@ extension NSSortDescriptor {
         NSSortDescriptor(keyPath: keyPath, ascending: true)
     }
     
+    public static func ascending<T, K>(
+        _ keyPath: KeyPath<T, K?>
+    ) -> NSSortDescriptor
+    where T: NSManagedObject,
+          K: Comparable
+    {
+        NSSortDescriptor(keyPath: keyPath, ascending: true)
+    }
+
     public static func descending<T, K>(
         _ keyPath: KeyPath<T, K>
+    ) -> NSSortDescriptor
+    where T: NSManagedObject,
+          K: Comparable
+    {
+        NSSortDescriptor(keyPath: keyPath, ascending: false)
+    }
+
+    public static func descending<T, K>(
+        _ keyPath: KeyPath<T, K?>
     ) -> NSSortDescriptor
     where T: NSManagedObject,
           K: Comparable
